c16e9a4a498dd832b2ae343f0914e37f08e5604a
Timoteo Soutello <tsoutello@gmail.com>
2021-01-09 15:53:14 -0300

    tiny adjusment

--- src/main/java/com/github/controller/UserClickController.java
+++ src/main/java/com/github/controller/UserClickController.java
@@ -1,13 +1,11 @@
 package com.github.controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.dto.UserClickDTO;
@@ -23,6 +21,6 @@ public class UserClickController {
     @PostMapping
     public ResponseEntity<?> save(@RequestBody UserClickDTO userClickDTO) {
 	userClickUsecase.includeUserTracking(userClickDTO);
-	return ResponseEntity.status(HttpStatus.OK);
+	return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
